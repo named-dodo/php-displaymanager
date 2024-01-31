e774e616fcb40119bd73f257b6e738768712c70a
named-dodo <namedrelay@protonmail.com>
2024-01-31 22:00:46 +0100

    Allow control-C to kill the application.

--- src/tty.php
+++ src/tty.php
@@ -3,7 +3,7 @@
 // enter terminal into semi-framebuffer mode...
 // escape sequences: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 function tty_disable(){
-	system("stty -echo raw"); // disable echo and input buffering.
+	system("stty -echo raw isig"); // disable echo and input buffering.
 	//echo("\033[0;0H"); // don't move to origin, it messes with the console on exit.
 	echo("\033[?25l\033[8m"); // turn off cursor visibility and text visibility.
 }
